2b08398138b2f9a41cb4af72c60cb21f46345735
MaPache07 <00170517@uca.edu.sv>
2019-06-02 21:09:48 -0600

    Se ha cambiado el icono del boton para añadir un partido, ademas de que se corregieron otros detalles de la interfaz

--- app/src/main/java/com/mapache/basketballcounter/MainActivity.kt
+++ app/src/main/java/com/mapache/basketballcounter/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity(), ListFragment.OnClickMatchListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             startActivity(Intent(this, NewMatchActivity::class.java))
@@ -53,21 +52,5 @@ class MainActivity : AppCompatActivity(), ListFragment.OnClickMatchListener {
     override fun OnClickBigMatchListener(match : Match) {
         var contentFragment = ContentFragment.newInstance(match)
         changeFragment(R.id.content_fragment, contentFragment)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
